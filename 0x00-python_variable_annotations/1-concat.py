0383352e4bfe6307c04b4ca93800c8916d07aef4
zulsb <zulsb2093@gmail.com>
2020-10-19 15:58:14 -0500

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Type-annotated function concat that takes a str arguments.
+        Args:
+            str1: str type.
+            str2: str type.
+        Return:
+            Concatenated string.
+    """
+    return str1 + str2
