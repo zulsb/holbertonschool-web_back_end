0497496fe471d65627b9bb432ffd072c730f8ed9
zulsb <zulsb2093@gmail.com>
2020-10-20 21:02:56 -0500

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Tasks """
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Async routine.
+        Args:
+            max_delay: integer argument.
+            n: integer argument.
+        Return:
+            List of all the delays random float.
+    """
+    delays: List[float] = []
+    orderList: List[float] = []
+
+    for i in range(n):
+        delays.append(wait_random(max_delay))
+
+    for o in asyncio.as_completed(delays):
+        orderList.append(await o)
+
+    return orderList
