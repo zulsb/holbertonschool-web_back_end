cba7630549a7a8d39388127e88d554ea858de490
zulsb <zulsb2093@gmail.com>
2020-11-04 23:45:19 -0500

    Simple-basic-API

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """sfsfsdf
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """dsdsdfs
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """sdffssf
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """sfffsd
+        """
+        return None
