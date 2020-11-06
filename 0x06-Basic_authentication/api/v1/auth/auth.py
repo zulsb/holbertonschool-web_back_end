c5d1a209b4d49632df19a924dd8cce001fc91fa0
zulsb <zulsb2093@gmail.com>
2020-11-05 20:36:24 -0500

    Auth class

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-"""
+"""fgdgdgdgdgfggf
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """sfsfsdf
+    """sfsfsdffggfhf
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """dsdsdfs
+        """dsdsdfsghfhfh
         """
         if path in excluded_paths:
             return False
 
     def authorization_header(self, request=None) -> str:
-        """sdffssf
+        """sdffssffghfh
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """sfffsd
+        """sfffsdfghf
         """
         return None
