39e59bbd22936a048d42304cc021be4b9bd150bf
zulsb <zulsb2093@gmail.com>
2020-11-05 23:31:54 -0500

    Routes authentication

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -15,9 +15,17 @@ class Auth:
                 path: String type.
                 excluded_paths: List type.
             Return:
-                False - path.
+                True if the path is not in the list of strings.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path in excluded_paths:
+            return False        
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method
