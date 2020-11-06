24070c7397c19e67c441733471b5e56ca1e02390
zulsb <zulsb2093@gmail.com>
2020-11-05 20:43:22 -0500

    Auth class - comments

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
-"""fgdgdgdgdgfggf
+""" Auth class.
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """sfsfsdffggfhf
+    """ Class to manage the API authentication.
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """dsdsdfsghfhfh
+        """ Method
+            Args:
+                path: String type.
+                excluded_paths: List type.
+            Return:
+                False - path.
         """
-        if path in excluded_paths:
-            return False
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """sdffssffghfh
+        """ Method
+            Args:
+                request.
+            Return:
+                None - request.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """sfffsdfghf
+        """ Method
+            Args:
+                request.
+            Return:
+                None - request.
         """
         return None
