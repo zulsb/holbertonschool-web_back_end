34a7a94a747bd2cbafad100ad5665d3a9bb62343
zulsb <zulsb2093@gmail.com>
2020-11-06 20:45:51 -0500

    Basic auth

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -18,6 +18,10 @@ if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+elif getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
 
 @app.before_request
 def beforeRequest() -> None:
