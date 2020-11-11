64a43bfdca2639b334bc29a0d9cd527f79e628f4
zulsb <zulsb2093@gmail.com>
2020-11-10 22:25:23 -0500

    Update users

--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -27,6 +27,8 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -42,6 +44,7 @@ def retrieveMe() -> str:
         User object JSON represented
     """
     return jsonify(request.current_user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
