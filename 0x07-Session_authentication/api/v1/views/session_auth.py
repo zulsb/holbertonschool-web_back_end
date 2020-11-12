53169f1c9db91f5edf3915ba3252cd6d74d5b4ca
zulsb <zulsb2093@gmail.com>
2020-11-11 21:31:00 -0500

    New view for Session Authentication

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+""" Views module that handles all
+    routes for the Session authentication.
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def logIn() -> str:
+    """ Return:
+            Dictionary representation of the User authenticated.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        users = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+
+            sessionID = auth.create_session(user_id=user.id)
+            response = jsonify(user.to_json())
+            response.set_cookie(getenv('SESSION_NAME'), sessionID)
+            return response
+    return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logOut() -> str:
+    """ Return:
+            Logout User authenticated.
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    else:
+        return False, abort(404)
