9c342db93217fa7c2fd789e2b878111577c9b487
zulsb <zulsb2093@gmail.com>
2020-11-18 00:01:26 -0500

    User model

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" SQLAlchemy model.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ Class that maps the Users table to store records for the users.
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
