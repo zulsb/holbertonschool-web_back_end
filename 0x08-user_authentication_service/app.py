a57bcb49cf9eb71c4086e6f0928066f68af2e158
zulsb <zulsb2093@gmail.com>
2020-11-18 20:14:28 -0500

    Basic Flask app

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" App module.
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def messageWelcome() -> str:
+    """ Return a JSON payload of the form.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
