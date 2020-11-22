40fd9240c5e99bd32609fc1d3945d93fbe3fa769
zulsb <zulsb2093@gmail.com>
2020-11-21 21:44:37 -0500

    Generate UUIDs

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 import bcrypt
 from db import DB
 from user import User
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
@@ -14,6 +15,12 @@ def _hash_password(password: str) -> str:
             A string.
     """
     return bcrypt.hashpw(bytes(password, "ascii"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Function that return a string representation of a new UUID.
+    """
+    return str(uuid4())
 
 
 class Auth:
