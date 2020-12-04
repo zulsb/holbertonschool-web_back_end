b4d7ae0a5302bcf0c68398b3f3f001c61453958c
zulsb <zulsb2093@gmail.com>
2020-12-04 01:28:14 -0500

    Create a table

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- Creates a table of users with id, email and name.
+-- Create a table of users with id, email and name.
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
