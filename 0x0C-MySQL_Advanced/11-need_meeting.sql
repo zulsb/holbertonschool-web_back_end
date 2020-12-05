41aa8017e0f6892ccee29acdfd9ba47b9b709194
zulsb <zulsb2093@gmail.com>
2020-12-04 23:18:10 -0500

    No table for a meeting

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,7 @@
 -- Create a view need_meeting that lists all students that have a score under 80.
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
+CREATE VIEW need_meeting
+AS
+    SELECT name
+    FROM students
+    WHERE score < 80 AND last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL
+1 MONTH);
