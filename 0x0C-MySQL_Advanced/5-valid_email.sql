85d3e992836256b0df4a5dccd9a8ce9556f1b181
zulsb <zulsb2093@gmail.com>
2020-12-04 21:20:47 -0500

    Email validation to sent

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Create a trigger that resets the attribute valid_email only when the email has been changed.
+DELIMITER $$
+CREATE TRIGGER email_correct BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF OLD.email <> NEW.email THEN
+	   SET NEW.valid_email = 0;
+	END IF;
+END;$$
+DELIMITER ;
