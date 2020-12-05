b07eb9b0c6c0bf27a03b98db7706471b963a6f63
zulsb <zulsb2093@gmail.com>
2020-12-04 22:58:37 -0500

    Average score

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,7 @@
+-- Create a stored procedure that computes and store the average score.
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+END;$$
+DELIMITER ;
