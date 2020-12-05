f1ecf81269b1ebc7220016f218bd70bf65fb7b98
zulsb <zulsb2093@gmail.com>
2020-12-04 23:06:41 -0500

    Safe divide

--- 0x0C-MySQL_Advanced/10-div.sql
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -2,10 +2,10 @@
 DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT
 BEGIN
-	IF b = 0 THEN
-		RETURN 0;
-	ELSE 
-	    RETURN a / b;
+    IF b = 0 THEN
+    RETURN 0;
+    ELSE
+    RETURN a / b;
     END IF;
 END;$$
 DELIMITER ;
