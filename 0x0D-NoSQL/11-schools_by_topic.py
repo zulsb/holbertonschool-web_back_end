b750b4af6fe3baf67785c65a0a50bf6ed6908136
zulsb <zulsb2093@gmail.com>
2020-12-08 23:21:23 -0500

    Topic searched

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Mongo change school topics module.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ Method that returns the list of school having a specific topic.
+        Arg:
+            mongo_collection: pymongo collection object.
+            topic: Topic searched.
+        Return:
+            A list.
+    """
+    return mongo_collection.find({"topics": {"$in": [topic]}})
