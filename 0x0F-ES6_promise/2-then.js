8e702c6e15e690b17eb5afe4f3caaf5faad0e3f5
zulsb <zulsb2093@gmail.com>
2020-12-16 01:17:52 -0500

    Then

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,6 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
     body: 'success',
-  })).catch(() => (new Error()))
-    .then(() => { console.log('Got a response from the API'); });
+  })).catch(() => (new Error())).finally(() => {
+    console.log('Got a response from the API');
+  });
 }
