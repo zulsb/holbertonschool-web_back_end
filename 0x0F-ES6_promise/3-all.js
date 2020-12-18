81f7226295cf756716a26c4913f7707931caa22b
zulsb <zulsb2093@gmail.com>
2020-12-17 21:18:22 -0500

    Multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return uploadPhoto().then((r) => {
-    createUser().then((val) => {
-      console.log(r.body, val.firstName, val.lastName);
-    }).catch(() => {
-      console.error('Signup system offline');
-    });
-  }).catch(() => {
-    console.error('Signup system offline');
-  });
+	return uploadPhoto().then((r) => {
+		createUser().then((val) => {
+			console.log(r.body, val.firstName, val.lastName);
+		}).catch(() => {
+			console.error('Signup system offline');
+		});
+	}).catch(() => {
+		console.error('Signup system offline');
+	});
 }
