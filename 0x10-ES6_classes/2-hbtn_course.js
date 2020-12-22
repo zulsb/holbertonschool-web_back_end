d2c4eac70c778b59f217b6a13d427407aeea6117
zulsb <zulsb2093@gmail.com>
2020-12-21 21:20:17 -0500

    A Course, Getters, and Setters

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,28 @@
+export default class ClassRoom {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw TypeError('Must be an array');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }  
+}
