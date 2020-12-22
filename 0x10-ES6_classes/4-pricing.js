fe2aa38a544d273aa71554dd6a252c22243e3d3f
zulsb <zulsb2093@gmail.com>
2020-12-21 21:40:35 -0500

    Pricing

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,25 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') throw TypeError('Amount must be a number');
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
