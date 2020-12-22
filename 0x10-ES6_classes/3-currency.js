544e81d816592a72dde96ff527d504d4ba518e58
zulsb <zulsb2093@gmail.com>
2020-12-21 21:23:46 -0500

    Methods

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,20 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') throw TypeError('Code must be a string');
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+	displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
