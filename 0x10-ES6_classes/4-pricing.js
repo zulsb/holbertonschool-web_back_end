3ce466686dc7eb466d354230a6cdd8977eb3bf7c
zulsb <zulsb2093@gmail.com>
2020-12-21 21:49:23 -0500

    Pricing

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,5 +1,3 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
     if (typeof amount !== 'number') throw TypeError('Amount must be a number');
