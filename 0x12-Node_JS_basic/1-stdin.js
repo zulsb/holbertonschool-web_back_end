4917ec951d749dc1e07271ceb36bb058e0589059
Luz Sánchez <zulsb2093@gmail.com>
2021-01-07 01:35:03 -0500

    try

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
 process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?\n');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
-  const yourname = process.stdin.read();
-  if (yourname !== null) {
-    process.stdout.write(`Your name is: ${yourname}`);
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
+
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
