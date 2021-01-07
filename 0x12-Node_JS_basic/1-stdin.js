c5814e138993b332aa247d8415962c7d80015cb1
Luz Sánchez <zulsb2093@gmail.com>
2021-01-07 01:27:39 -0500

    Fix

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
 process.stdin.setEncoding('utf8');
-console.write('Welcome to Holberton School, what is your name?');
+
+console.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
   const yourName = process.stdin.read();
   if (yourName !== null) {
     process.stdout.write(`Your name is: ${yourName}`);
   }
 });
+
 process.stdin.on('end', () => {
   console.write('This important software is now closing\n');
 });
