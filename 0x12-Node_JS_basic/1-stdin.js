fbd65c3cb7873915d4c84ce51aca820c3cd5cceb
Luz Sánchez <zulsb2093@gmail.com>
2021-01-07 01:42:50 -0500

    hgfhf

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
 process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?\n');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
   const n = process.stdin.read();
   if (n !== null) {
     process.stdout.write(`Your name is: ${n}`);
   }
 });
+
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
