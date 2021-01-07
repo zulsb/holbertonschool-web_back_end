45b9a5abba5a33e8eb4c3bf350b4a59ddb8e1bd5
Luz Sánchez <zulsb2093@gmail.com>
2021-01-07 01:39:22 -0500

    Spaces

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,14 +1,11 @@
 process.stdin.setEncoding('utf8');
-
 console.log('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.on('readable', () => {
   const n = process.stdin.read();
   if (n !== null) {
     process.stdout.write(`Your name is: ${n}`);
   }
 });
-
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
