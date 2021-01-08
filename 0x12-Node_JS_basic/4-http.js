1b6a2871699c2070053dd38d5e300c454e296218
zulsb <zulsb2093@gmail.com>
2021-01-08 01:15:16 -0500

    Try

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-	res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port);
