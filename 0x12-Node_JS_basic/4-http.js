cebe811e521ebce2932d2de299a5e9765ac0646f
zulsb <zulsb2093@gmail.com>
2021-01-08 01:07:40 -0500

    Try

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;
