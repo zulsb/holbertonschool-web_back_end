bb78ccd61e7d160b77f764591f7fe699586e2eee
zulsb <zulsb2093@gmail.com>
2021-01-08 19:53:58 -0500

    complex HTTP server

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const students = require('./3-read_file_async');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      students(process.argv[2])
+        .then((data) => {
+          res.end(data);
+        })
+        .catch((error) => {
+          res.end(error.message);
+        });
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Resource not found' }));
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
