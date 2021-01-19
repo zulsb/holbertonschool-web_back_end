dc099fd8c3397f927dd21201d15a0f8194bd8be0
zulsb <zulsb2093@gmail.com>
2021-01-19 02:53:43 -0500

    Throw an error

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (errors) {
+      queue.push(`${errors.name}: ${errors.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
