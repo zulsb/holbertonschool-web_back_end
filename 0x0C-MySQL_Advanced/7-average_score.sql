b94f62600268b221631365833850f5ae6fcdecef
zulsb <zulsb2093@gmail.com>
2021-01-18 20:19:59 -0500

    Set fix

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -3,5 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
 	UPDATE users
+	SET average_score = (SELECT SUM(score) / COUNT(*) FROM corrections AS c WHERE c.user_id = user_id)
+	WHERE id = user_id;
 END;$$
 DELIMITER ;
