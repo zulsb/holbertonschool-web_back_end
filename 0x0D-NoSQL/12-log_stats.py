6d0385c374c1adbd9bed7076064a17f27d6074ee
zulsb <zulsb2093@gmail.com>
2021-01-18 21:01:47 -0500

    Log stats fix

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://localhost:27017')
     collec = client.logs.nginx
 
     print(f'{collec.count_documents({})} logs')
