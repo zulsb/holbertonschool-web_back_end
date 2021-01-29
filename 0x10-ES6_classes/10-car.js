4d112af65b1c5f5fd4316563d9e0e455fce92187
zulsb <zulsb2093@gmail.com>
2021-01-28 23:37:02 -0500

    Vromm [Fix]

--- 0x10-ES6_classes/10-car.js
+++ 0x10-ES6_classes/10-car.js
@@ -4,4 +4,13 @@ export default class Car {
     this._motor = motor;
     this._color = color;
   }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
 }
