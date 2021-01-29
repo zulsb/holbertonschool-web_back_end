a3260ed94283271214bce436f8ed83b1ee3d51a6
zulsb <zulsb2093@gmail.com>
2021-01-28 23:24:21 -0500

    Fix import pricing

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,3 +1,5 @@
+import Currency from './3-currency';
+
 export default class Pricing {
   constructor(amount, currency) {
     if (typeof amount !== 'number') throw TypeError('Amount must be a number');
