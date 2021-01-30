070f8bdca888b68fe12a5f36d221eaf242dd9728
zulsb <zulsb2093@gmail.com>
2021-01-30 00:22:34 -0500

    Combine

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,8 @@
 export default function updateStudentGradeByCity(listS, city, newGrades) {
-  return listS.filter(
-    (student) => student.location === city
-  )
+  return listStudents.filter(
+    (student) => student.location === city,
+  ).map((student) => Object.assign(
+    student,
+    { grade: newGrades.filter((grade) => student.id === grade.studentId)[0] ? newGrades.filter((grade) => student.id === grade.studentId)[0].grade : 'N/A' },
+  ));
 }
